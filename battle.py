1fe675b4e658d5eee51db20324743aebcdcd9391
rolika <weisz.roland@pohlen-dach.hu>
2015-09-10 22:45:25 +0200

    created base classes

---
+++ battle.py
@@ -0,0 +1,64 @@
+#BattleShip game on console
+
+import random
+
+class Ocean(list):
+    """ Create the ocean """
+    def __init__(self, rows = 20, columns = 40):
+        super().__init__(self)
+        self.rows, self.columns = rows, columns
+        self._create()
+
+    def _create(self):
+        """ Create data field, a list of lists """
+        for row in range(self.rows):
+            self.append([])
+            for column in range(self.columns):
+                self[row].append([])
+                self[row][column] = random.choice("~˛") #AltGr+1, AltGr+6
+
+    def get(self, row):
+        """ Return row of ocean as string """
+        return ''.join(self[row])
+
+class Board:
+    """ Create game board based on ocean """
+    def __init__(self, ocean): #takes ocean as object
+        self._ocean = ocean
+        self._full_row = " {:>2} {} {}" #left row-num, ocean-row, right row-num
+        self._horizontal_nums = \
+            ''.join([str(num % 10) for num in range(ocean.columns)])
+        self._horizontal_tens = \
+            (' ' * 9).join([str(num) for num in range(ocean.columns // 10)])
+        self.draw()
+
+    def draw(self):
+        """ Draw board with coordinates around """
+        print(self._full_row.format("", self._horizontal_tens, ""))
+        print(self._full_row.format("", self._horizontal_nums, ""))
+        for row in range(self._ocean.rows):
+            print(self._full_row.format(row, self._ocean.get(row), row))
+        print(self._full_row.format("", self._horizontal_nums, ""))
+        print(self._full_row.format("", self._horizontal_tens, ""))
+
+class Ship:
+    """ Create a ship """
+    def __init__(self, shape): #shape is a multiline string
+        self.shape = shape.strip('\n').split('\n')
+
+    def getCoords(self):
+        """ Return relative coordinates of shape """
+        coords = []
+        for y in len(self.shape): #going row for row
+            for x in max(self.shape):
+                pass
+
+ocean = Ocean()
+board = Board(ocean)
+s = """
+  ###
+######
+ ###
+"""
+ship = Ship(s)
+print(ship.getCoords)
