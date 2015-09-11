309501834d12d1bf126ef92acd5d90f1d56a88b1
rolika <weisz.roland@pohlen-dach.hu>
2015-09-11 22:52:51 +0200

    added many stuff

--- battle.py
+++ battle.py
@@ -2,11 +2,36 @@
 
 import random
 
+SHIPS = [ \
+"""
+###   ###
+   ###
+   ###
+###   ###
+""",
+"""
+   ###
+ #######
+#########
+""",
+"""
+#####
+#####
+#####
+""",
+"""
+  ###
+######
+ ###
+""" ]
+INTACT, HIT, MISS, SUNKEN = '#', '+', 'o', '×'
+
 class Ocean(list):
     """ Create the ocean """
     def __init__(self, rows = 20, columns = 40):
         super().__init__(self)
         self.rows, self.columns = rows, columns
+        self.bounds = list() #list of ships upper left & lower right coordinates
         self._create()
 
     def _create(self):
@@ -21,6 +46,23 @@ class Ocean(list):
         """ Return row of ocean as string """
         return ''.join(self[row])
 
+    def addShips(self, ships):
+        """ Randomly place ships in ocean """
+        for ship in ships:
+            ship.set(self.getCoord())
+
+    def getCoord(self, wide, long):
+        """ Return appropriate coordinates for ship """
+        new = False
+        while not new:
+            new = True
+            x = random.randrange(self.columns - long)
+            y = random.randrange(self.rows - wide)
+            for bound in self.bounds:
+                if x in range(x, x + long) and y in range(y, y + wide):
+                    new = False            
+        return x, y
+
 class Board:
     """ Create game board based on ocean """
     def __init__(self, ocean): #takes ocean as object
@@ -30,8 +72,7 @@ class Board:
             ''.join([str(num % 10) for num in range(ocean.columns)])
         self._horizontal_tens = \
             (' ' * 9).join([str(num) for num in range(ocean.columns // 10)])
-        self.draw()
-
+        
     def draw(self):
         """ Draw board with coordinates around """
         print(self._full_row.format("", self._horizontal_tens, ""))
@@ -41,26 +82,54 @@ class Board:
         print(self._full_row.format("", self._horizontal_nums, ""))
         print(self._full_row.format("", self._horizontal_tens, ""))
 
-class Ship:
+class Ship(dict):
     """ Create a ship """
     def __init__(self, shape): #shape is a multiline string
+        super().__init__(self)
         self._shape = shape.strip('\n').split('\n')
-            
-    def coords(self):
-        """ Return relative coordinates of shape """
-        coords = list()
-        for y, row in enumerate(self._shape):
+        self._coords = list()
+        for y, row in enumerate(self._shape): #get coords of ship-tiles
             for x, c in enumerate(row):
                 if c == "#":
-                    coords.append((x, y))
-        return coords
+                    self._coords.append([x, y])
+        self.wide = len(self._shape)
+        self.long = max(self._coords)[0]
 
-ocean = Ocean()
-board = Board(ocean)
-s = """
-  ###
-######
- ###
-"""
-ship = Ship(s)
-print(ship.coords())
+    def set(self, x, y):
+        """ Set intact ship to given coordinates on ocean """
+        for sx, sy in self._coords:
+            self[(sx + x, sy + y)] = INTACT
+
+    def hit(self, x, y):
+        """ Ship-tile hit by torpedo """
+        self[(x, y)] = HIT
+
+    def sunken(self):
+        """ Check and redraw sunken ship """
+        for coord in self:
+            if self[coord] == INTACT: #at least one intact tile
+                return False
+        for coord in self: #else redraw
+            self[coord] = SUNKEN
+        return True
+
+class BattleShip:
+    """ Main game application """
+    def __init__(self):
+        self.initGame()
+        self.newGame()
+
+    def initGame(self):
+        """ Init new game """
+        self.ocean = Ocean()
+        self.board = Board(self.ocean)
+        self.ships = [Ship(ship) for ship in SHIPS]
+        self.ocean.addShips(self.ships)
+
+    def newGame(self):
+        """ Start new game """
+        print("{:_^48}".format("_".join(list("torpedó".upper()))))
+        self.board.draw()
+        print(self.ships[0].wide, self.ships[0].long)
+        
+app = BattleShip()
