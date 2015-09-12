7d87e57604622f08c1496ecfeb4942257d96655c
rolika <weisz.roland@pohlen-dach.hu>
2015-09-12 22:32:19 +0200

    something completely different

--- battle.py
+++ battle.py
@@ -2,6 +2,7 @@
 
 import random
 
+WAVE, INTACT, HIT, MISS, SUNKEN = "~˛", '#', '+', 'o', '×'
 SHIPS = [ \
 """
 ###   ###
@@ -24,44 +25,48 @@ SHIPS = [ \
 ######
  ###
 """ ]
-INTACT, HIT, MISS, SUNKEN = '#', '+', 'o', '×'
 
-class Ocean(list):
-    """ Create the ocean """
+class Map(list):
+    """ Base class to create a list of list holding the maps for:
+        - ocean representing game field
+        - master map holding the ships position """
     def __init__(self, rows = 20, columns = 40):
-        super().__init__(self)
+        super().__init__()
         self.rows, self.columns = rows, columns
-        self.bounds = list() #list of ships upper left & lower right coordinates
-        self._create()
-
-    def _create(self):
-        """ Create data field, a list of lists """
-        for row in range(self.rows):
+        for row in range(rows):
             self.append([])
-            for column in range(self.columns):
+            for column in range(columns):
                 self[row].append([])
-                self[row][column] = random.choice("~˛") #AltGr+1, AltGr+6
+
+class Ocean(Map):
+    """ Create the ocean, ie. the actual game board to display """
+    def __init__(self):
+        super().__init__()
+        for row in self:
+            for column in row:
+                column.append(random.choice(WAVE))
+
+    def __str__(self):
+        """ Map's string representation for debug purposes """
+        return '\n'.join(map(lambda lst: ''.join([l[0] for l in lst]), self))
 
     def get(self, row):
         """ Return row of ocean as string """
-        return ''.join(self[row])
+        return ''.join([field[0] for field in self[row]])
 
-    def addShips(self, ships):
-        """ Randomly place ships in ocean """
-        for ship in ships:
-            ship.set(self.getCoord())
+class Master(Map):
+    """ Master map containing ships """
+    def __init__(self):
+        super().__init__()
 
-    def getCoord(self, wide, long):
-        """ Return appropriate coordinates for ship """
-        new = False
-        while not new:
-            new = True
-            x = random.randrange(self.columns - long)
-            y = random.randrange(self.rows - wide)
-            for bound in self.bounds:
-                if x in range(x, x + long) and y in range(y, y + wide):
-                    new = False            
-        return x, y
+    def __str__(self):
+        """ Map's string representation for debug purposes """
+        return '\n'.join(map(lambda lst: ''.join([l[0] for l in lst]), self))
+
+    def add(self, ship, x, y):
+        """ Place ship on master map at given coordinates """
+        for sx, sy in ship:
+            self[x + sx][y + sy] = ship[(sx, sy)]
 
 class Board:
     """ Create game board based on ocean """
@@ -72,7 +77,7 @@ class Board:
             ''.join([str(num % 10) for num in range(ocean.columns)])
         self._horizontal_tens = \
             (' ' * 9).join([str(num) for num in range(ocean.columns // 10)])
-        
+
     def draw(self):
         """ Draw board with coordinates around """
         print(self._full_row.format("", self._horizontal_tens, ""))
@@ -117,19 +122,22 @@ class BattleShip:
     """ Main game application """
     def __init__(self):
         self.initGame()
-        self.newGame()
+        #self.newGame()
 
     def initGame(self):
         """ Init new game """
         self.ocean = Ocean()
+        print(self.ocean)
+        print(self.ocean[0][0][0])
+        self.master = Master()
         self.board = Board(self.ocean)
         self.ships = [Ship(ship) for ship in SHIPS]
-        self.ocean.addShips(self.ships)
+        self.master.add(self.ships[0], 0, 0)
+        #print(self.master)
 
     def newGame(self):
         """ Start new game """
-        print("{:_^48}".format("_".join(list("torpedó".upper()))))
+        print("{:_^48}".format(' '.join(list(" torpedó ".upper()))))
         self.board.draw()
-        print(self.ships[0].wide, self.ships[0].long)
-        
+
 app = BattleShip()
