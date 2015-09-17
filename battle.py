7ff647216e1337407849c92d7583fcb8072cc051
rolika <weisz.roland@pohlen-dach.hu>
2015-09-17 20:14:18 +0200

    add ship to master map

--- battle.py
+++ battle.py
@@ -2,7 +2,7 @@
 
 import random
 
-WAVE, INTACT, HIT, MISS, SUNKEN = "~˛", '#', '+', 'o', '×'
+WAVE, BODY, HIT, MISS, SUNKEN = "~˛", '#', '+', 'o', '×'
 SHIPS = [ \
 """
 ###   ###
@@ -36,7 +36,11 @@ class Map(list):
         for row in range(rows):
             self.append([])
             for column in range(columns):
-                self[row].append('')
+                self[row].append(' ') #needs to be space, not empty string
+                
+    def __str__(self):
+        """ Map's string representation for debug purposes """
+        return '\n'.join(map(lambda row: ''.join([c for c in row]), self))
 
 class Ocean(Map):
     """ Create the ocean, ie. the actual game board to display """
@@ -45,11 +49,6 @@ class Ocean(Map):
         for row in self:
             for i, column in enumerate(row):
                 row[i] = random.choice(WAVE)
-
-    def __str__(self):
-        """ Map's string representation for debug purposes """
-        return '\n'.join([self.get(row) for row in range(len(self))])
-        #return '\n'.join(map(lambda row: ''.join([c for c in row]), self))
 
     def get(self, row):
         """ Return row of ocean as string """
@@ -60,14 +59,10 @@ class Master(Map):
     def __init__(self):
         super().__init__()
 
-    def __str__(self):
-        """ Map's string representation for debug purposes """
-        return '\n'.join(map(lambda lst: ''.join([l[0] for l in lst]), self))
-
     def add(self, ship, x, y):
         """ Place ship on master map at given coordinates """
         for sx, sy in ship:
-            self[x + sx][y + sy] = ship[(sx, sy)]
+            self[sy + y][sx + x] = BODY
 
 class Board:
     """ Create game board based on ocean """
@@ -88,57 +83,37 @@ class Board:
         print(self._full_row.format("", self._horizontal_nums, ""))
         print(self._full_row.format("", self._horizontal_tens, ""))
 
-class Ship(dict):
-    """ Create a ship """
+class Ship(list):
+    """ Create a ship with its relative coordinates """
     def __init__(self, shape): #shape is a multiline string
         super().__init__(self)
         self._shape = shape.strip('\n').split('\n')
-        self._coords = list()
         for y, row in enumerate(self._shape): #get coords of ship-tiles
             for x, c in enumerate(row):
-                if c == "#":
-                    self._coords.append([x, y])
-        self.wide = len(self._shape)
-        self.long = max(self._coords)[0]
-
-    def set(self, x, y):
-        """ Set intact ship to given coordinates on ocean """
-        for sx, sy in self._coords:
-            self[(sx + x, sy + y)] = INTACT
-
-    def hit(self, x, y):
-        """ Ship-tile hit by torpedo """
-        self[(x, y)] = HIT
-
-    def sunken(self):
-        """ Check and redraw sunken ship """
-        for coord in self:
-            if self[coord] == INTACT: #at least one intact tile
-                return False
-        for coord in self: #else redraw
-            self[coord] = SUNKEN
-        return True
+                if c == BODY:
+                    self.append((x, y))
+        self.wide = len(self._shape) #wide & long for placing on map
+        self.long = max(self)[0]
+        self.count = len(self) #hits to sink ship
 
 class BattleShip:
     """ Main game application """
     def __init__(self):
         self.initGame()
-        #self.newGame()
+        self.newGame()
 
     def initGame(self):
         """ Init new game """
         self.ocean = Ocean()
-        print(self.ocean)
-##        print(self.ocean[0][0][0])
-##        self.master = Master()
-##        self.board = Board(self.ocean)
-##        self.ships = [Ship(ship) for ship in SHIPS]
-##        self.master.add(self.ships[0], 0, 0)
-##        print(self.master)
+        self.master = Master()
+        self.board = Board(self.ocean)
+        self.ships = [Ship(ship) for ship in SHIPS]
+        self.master.add(self.ships[0], 1, 1)
+        print(self.master)
 
     def newGame(self):
         """ Start new game """
-        print("{:_^48}".format(' '.join(list(" torpedó ".upper()))))
+        print("{:_^48}".format(' '.join(list("torpedó".upper()))))
         self.board.draw()
 
 app = BattleShip()
