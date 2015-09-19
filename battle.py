e19382c668d7d2248d1932b7bfecdf1060c421b3
rolika <weisz.roland@pohlen-dach.hu>
2015-09-19 18:18:32 +0200

    non-overlapping random ship placing

--- battle.py
+++ battle.py
@@ -6,8 +6,8 @@ WAVE, VESSEL, HIT, MISS, SUNKEN = "~˛", '#', '+', 'o', '×'
 SHIPS = [ \
 """
 ###   ###
-   ###
-   ###
+  #####
+  #####
 ###   ###
 """,
 """
@@ -16,9 +16,9 @@ SHIPS = [ \
 #########
 """,
 """
-#####
-#####
-#####
+#######
+#######
+#######
 """,
 """
   ###
@@ -27,56 +27,53 @@ SHIPS = [ \
 """ ,
 """
  ####
-##  ###
-##   ###
-##  ###
+#######
+########
+#######
  ####
+""",
+"""
+#######
+#     #
+#     #
+#######
 """]
 
 class Map(list):
-    """ Base class to create a list of list holding the maps for:
-        - ocean representing game field
-        - master map holding the ships position """
     def __init__(self, rows = 17, columns = 40):
         super().__init__()
         self.rows, self.columns = rows, columns
-        for row in range(rows):
-            self.append([])
-            for column in range(columns):
-                self[row].append(' ') #needs to be space, not empty string
-                
-    def __str__(self):
-        """ Map's string representation for debug purposes """
-        return '\n'.join(map(lambda row: ''.join([c for c in row]), self))
+        self.create_dataset()
+        self.create_ocean()
 
-class Ocean(Map):
-    """ Create the ocean, ie. the actual game board to display """
-    def __init__(self):
-        super().__init__()
+    def create_dataset(self):
+        """ Create rows and in each row, all the columns """
+        for row in range(self.rows):
+            self.append([])
+            for column in range(self.columns):
+                self[row].append('')
+
+    def create_ocean(self):
+        """ Fill ocean with wave symbols """
         for row in self:
             for i, column in enumerate(row):
                 row[i] = random.choice(WAVE)
 
     def get(self, row):
-        """ Return row of ocean as string """
+        """ Return a row as string for the game board """
         return ''.join(self[row])
 
-class Master(Map):
-    """ Master map containing ships """
-    def __init__(self):
-        super().__init__()
+    def add(self, ship):
+        """ Place ship on map for debug purposes """
+        try:
+            for coord in ship:
+                self[coord[0]][coord[1]] = VESSEL
+        except:
+            print(ship.coords)
 
-    def add(self, ship, x, y):
-        """ Place ship on master map at given coordinates """
-        for sx, sy in ship:
-            self[sy + y][sx + x] = VESSEL
-
-    def empty(self, ship, x, y):
-        """ Check if ship can placed there """
-        for sx, sy in ship:
-            if self[sy + y][sx + x] == VESSEL:
-                return False
-        return True
+    def __str__(self):
+        """ Map's string representation for debug purposes """
+        return '\n'.join(map(lambda row: ''.join([c for c in row]), self))
 
 class Board:
     """ Create game board based on ocean """
@@ -97,44 +94,69 @@ class Board:
         print(self._full_row.format("", self._horizontal_nums, ""))
         print(self._full_row.format("", self._horizontal_tens, ""))
 
-class Ship(list):
-    """ Create a ship with its relative coordinates """
+class Ship(dict):
+    """ Create a ship with its coordinates """
     def __init__(self, shape): #shape is a multiline string
         super().__init__(self)
-        self._shape = shape.strip('\n').split('\n')
-        for y, row in enumerate(self._shape): #get coords of ship-tiles
-            for x, c in enumerate(row):
+        shape = shape.strip('\n').split('\n')
+        self.coords = self.get_coords(shape)
+
+    def get_coords(self, shape):
+        """ Make a list of coordinate pairs """
+        coords = list()
+        for x, row in enumerate(shape):
+            for y, c in enumerate(row):
                 if c == VESSEL:
-                    self.append((x, y))
-        self.wide = len(self._shape) #wide & long for placing on map
-        self.long = max(self)[0]
-        self.count = len(self) #hits to sink ship
+                    coords.append([x, y])
+        self.wide = len(shape) #wide & long for placing on map
+        self.long = max(max(zip(*coords))) + 1
+        return coords
+
+    def check(self):
+        """ Check if ship sunken - all vessel parts hit """
+        return False if VESSEL in self.values() else True
 
 class BattleShip:
     """ Main game application """
     def __init__(self):
-        self.initGame()
-        self.newGame()
+        self.init_game()
+        for ship in self.ships:
+            self.ocean.add(ship)
+        print(self.ocean)
+        self.new_game()
 
-    def initGame(self):
+    def init_game(self):
         """ Init new game """
-        self.ocean = Ocean()
-        self.master = Master()
+        self.ocean = Map()
         self.board = Board(self.ocean)
-        self.ships = [Ship(ship) for ship in SHIPS]        
-        for ship in self.ships: #add ships to master map
-            while True:
-                x = random.randrange(self.master.columns - ship.long)
-                y = random.randrange(self.master.rows - ship.wide)
-                if self.master.empty(ship, x, y):
-                    break
-            self.master.add(ship, x, y)
-        self.torpedos = sum([ship.count for ship in self.ships]) * 3
+        self.ships = [Ship(ship) for ship in SHIPS]
+        for ship in self.ships:
+            self.add(ship)
+        self.torpedos = 3
 
-    def newGame(self):
+    def add(self, ship):
+        """ Add ship to ocean, i.e. translate its coordinates """
+        while True:
+            x = random.randrange(self.ocean.rows - ship.wide)
+            y = random.randrange(self.ocean.columns - ship.long)
+            coords =[[coord[0] + x, coord[1] + y] for coord in ship.coords]
+            if not self.overlap(coords):
+                for coord in coords:
+                    ship[tuple(coord)] = VESSEL
+                break
+
+    def overlap(self, coords):
+        """ Check coordinates already taken """
+        for ship in self.ships:
+            for coord in ship:
+                if list(coord) in coords:
+                    return True
+        return False
+
+    def new_game(self):
         """ Start new game """
-        print("{:_^48}".format(' '.join(list("torpedó".upper()))))
         while True: #main game loop
+            print("{:_^48}".format(' '.join(list("torpedó".upper()))))
             self.board.draw()
             print("{} torpedód van.".format(self.torpedos), end = " ")
             try:
@@ -150,5 +172,10 @@ class BattleShip:
             print("Korrekt értékek")
             break
 
+def test():
+    s = Ship(SHIPS[4])
+    print(s.coords)
+
 if __name__ == "__main__":
     app = BattleShip()
+    #test()
